ed5baf09db3eefa96b04f3e231e2ee79933ba3c1
Jetsuko <jessean.infiltrator@gmail.com>
2022-06-08 17:02:33 -0700

    initial commit

---
+++ public/scripts/main.js
@@ -0,0 +1,29 @@
+const LIKE_BUTTONS = Array.from(document.querySelectorAll('.like'));
+
+LIKE_BUTTONS.forEach(async likeButton => {
+    likeButton.addEventListener('click', async e => {
+        const liBar = e.currentTarget.closest('.bar');
+        const barLyrics = liBar.querySelector('blockquote').innerText;
+        const barLikes = Number(liBar.querySelector('.barLikes').innerText) + 1;
+        const barRapper = liBar.querySelector('.barRapper').innerText;
+
+        try {
+            const response = await fetch('addLike', {
+                method: 'put',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    barLyrics,
+                    barRapper,
+                    barLikes
+                })
+            });
+            const data = await response.json();
+
+            location.reload();
+        }
+        catch (error) {
+            console.error(error);
+        }
+        
+    });
+});
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const connectionString = process.env.CONNECTIONSTRING;
+
+MongoClient.connect(connectionString)
+    .then(client => {
+        const db = client.db('rapper-api');
+
+        app.set('view engine', 'ejs');
+        app.use(express.static('public'));
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
+
+        app.get('/', (request, response) => {
+            // console.log(request);
+            db.collection('rappers').find().toArray()
+                .then(data => {
+                    response.render('index.ejs', { bars: data });
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
+
+        app.post('/addBar', (request, response) => {
+            let bar = request.body;
+            db.collection('rappers').insertOne({
+                barLyrics: bar.barLyrics,
+                barRapper: bar.barRapper,
+                barSong: bar.barSong,
+                barLikes: 0
+            })
+                .then(result => {
+                    console.log('Bar added');
+                    response.redirect('/');
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
+
+        app.put('/addLike', (request, response) => {
+            db.collection('rappers').findOneAndUpdate(
+                { barLyrics: request.body.barLyrics },
+                {
+                    $set: {
+                        barLyrics: request.body.barLyrics,
+                        barRapper: request.body.barRapper,
+                        barLikes: request.body.barLikes
+                    }
+                },
+                {
+                    upsert: true
+                }
+                )
+                .then(result => {
+                    console.log(result);
+                    response.json(result);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
+
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        });
+    })
+    .catch(err => console.error(err));
